207062583482b6e008d55ae72afd38e73475aa0e
YukenNarumi <hassiba.cc2@gmail.com>
2018-10-29 15:18:27 +0900

    ・修正:コード整理 　コメント追記 　警告が発生していた個所の一部対応

--- app/src/main/java/com/example/yuken/musicplayer/ImageCache.java
+++ app/src/main/java/com/example/yuken/musicplayer/ImageCache.java
@@ -5,8 +5,14 @@ import android.graphics.Bitmap;
 import java.util.HashMap;
 
 public class ImageCache {
-    private static HashMap<String, Bitmap> cache = new HashMap<String, Bitmap>();
+    private static HashMap<String, Bitmap> cache = new HashMap<>();
 
+    /**
+     * キャッシュされている画像を取得
+     *
+     * @param key キャッシュのキー
+     * @return キャッシュされている場合画像 / されていない場合 null
+     */
     public static Bitmap getImage(String key) {
         if (cache.containsKey(key)) {
             return cache.get(key);
@@ -14,6 +20,12 @@ public class ImageCache {
         return null;
     }
 
+    /**
+     * 画像をキャッシュする
+     *
+     * @param key   キャッシュのキー
+     * @param image キャッシュする画像
+     */
     public static void setImage(String key, Bitmap image) {
         cache.put(key, image);
     }
--- app/src/main/java/com/example/yuken/musicplayer/ImageGetTask.java
+++ app/src/main/java/com/example/yuken/musicplayer/ImageGetTask.java
@@ -1,30 +1,54 @@
 package com.example.yuken.musicplayer;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.widget.ImageView;
 
 public class ImageGetTask extends AsyncTask<String, Void, Bitmap> {
+    // アルバムアート用画像サイズ
+    private final int ALBUM_ART_IMAGE_WIDTH  = 72;
+    private final int ALBUM_ART_IMAGE_HEIGHT = 72;
+
+    @SuppressLint("StaticFieldLeak")
     private ImageView image;
     private String    tag;
 
-    public ImageGetTask(ImageView _image) {
+    /**
+     * デフォルトコンストラクタ
+     *
+     * @param _image 表示する画像
+     */
+    ImageGetTask(ImageView _image) {
         super();
         image = _image;
         tag = image.getTag().toString();
     }
 
+    /**
+     * 非同期処理
+     * 画像の取得 / キャッシュ済みの場合はキャッシュから、そうでない場合は通常読み込み
+     *
+     * @param params 画像パス
+     * @return 非同期処理で取得した画像
+     */
     @Override
     protected Bitmap doInBackground(String... params) {
         Bitmap bitmap = ImageCache.getImage(params[0]);
         if (bitmap == null) {
-            bitmap = decodeBitmap(params[0], 72, 72);
+            bitmap = decodeBitmap(params[0], ALBUM_ART_IMAGE_WIDTH, ALBUM_ART_IMAGE_HEIGHT);
             ImageCache.setImage(params[0], bitmap);
         }
         return bitmap;
     }
 
+    /**
+     * 非同期処理終了後に実行する処理
+     * 非同期処理で取得した画像を設定する
+     *
+     * @param result 非同期処理で取得した画像
+     */
     @Override
     protected void onPostExecute(Bitmap result) {
         if (tag.equals(image.getTag())) {
@@ -32,7 +56,15 @@ public class ImageGetTask extends AsyncTask<String, Void, Bitmap> {
         }
     }
 
-    public static Bitmap decodeBitmap(String path, int width, int height) {
+    /**
+     * Bitmap形式への画像デコード
+     *
+     * @param path   画像パス
+     * @param width  画像の幅
+     * @param height 画像の高さ
+     * @return 画像パスから読み込んだ画像 / 画像が存在しない場合 null
+     */
+    private static Bitmap decodeBitmap(String path, int width, int height) {
         final BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(path, options);
@@ -41,7 +73,15 @@ public class ImageGetTask extends AsyncTask<String, Void, Bitmap> {
         return BitmapFactory.decodeFile(path, options);
     }
 
-    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    /**
+     * 画像をサブサンプル(縮小して読み込む、低解像度で読み込む)するためのサイズ計算
+     *
+     * @param options   デコーダ用オプション
+     * @param reqWidth  画像の幅
+     * @param reqHeight 画像の高さ
+     * @return 2のべき乗となるよう計算したサイズ
+     */
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
         final int height       = options.outHeight;
         final int width        = options.outWidth;
         int       inSampleSize = 1;
